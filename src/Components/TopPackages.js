c65f49675c3a05447338ae50fd9a8dadd1a71091
rajesh <rajesh@appbroda.com>
2024-02-11 14:02:51 +0530

    added header

--- src/Components/TopPackages.js
+++ src/Components/TopPackages.js
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { useMemo } from 'react';
 
 const TopPackages = ({ packages }) => {
   const myPackage = useMemo(() => {
@@ -9,18 +9,24 @@ const TopPackages = ({ packages }) => {
   return (
     <div>
       <h1>
-        <span className="badge badge-pill badge-secondary m-3">
+        <span className='badge badge-pill badge-secondary m-3'>
           Top 10 packages
         </span>
       </h1>
-      <ul className="list-group">
+      <ul className='list-group'>
+        <li className='list-group-item d-flex justify-content-between align-items-center'>
+          Package Name
+          <h4>
+            <span className='badge badge-secondry'>Package Count</span>
+          </h4>
+        </li>
         {myPackage.map((pack) => (
           <li
-            className="list-group-item d-flex justify-content-between align-items-center"
+            className='list-group-item d-flex justify-content-between align-items-center'
             id={pack.key}
           >
             {pack.key}
-            <span className="badge badge-primary badge-pill">{pack.count}</span>
+            <span className='badge badge-primary badge-pill'>{pack.count}</span>
           </li>
         ))}
       </ul>
